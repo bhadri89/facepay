e19b9e8a24863ade1be617a0f0ec5d3592dd8313
bhadri89 <bhadreshn@gmail.com>
2019-04-11 14:47:12 +0530

    second commit

--- JS/loginController.js
+++ JS/loginController.js
@@ -1,20 +1,20 @@
-app.controller("loginController", ['$scope', '$state','$http',function($scope, $state,$http) {
+app.controller("loginController", ['$scope', '$state','$http','$rootScope',function($scope, $state,$http,$rootScope) {
 	$scope.user = {};
 	$scope.user.autherror = "";
 	$scope.user.authenticationError = false;
 	$scope.submitForLogin = function(){
 		$scope.user.autherror = "";
 		$scope.user.authenticationError = false;
-		var data = {
-				"user_id":$scope.user.userName,
-				"pwd":$scope.user.password
-		}
-		$http.post('http://BLR1-LTJHHMQN2:4344/frsEngine/merchent/login',JSON.stringify(data)).then(function (response) {
-			if(response.success_status){
-			 	$scope.user.user_id = response.user_id;
-        		$scope.user.business_name = response.business_name;
-        		$scope.user.owner_name = response.owner_name;
-        		$scope.user.bank_acc = response.bank_acc;
+		var myMap = new Map();
+        myMap.set('user_id', $scope.user.userName);
+        myMap.set('pwd', $scope.user.password);
+		$http.post('http://BLR1-LTJHHMQN2:4344/frsEngine/merchent/login',JSON.stringify(myMap)).then(function (response) {
+			if(response.data.success_status){
+			 	$scope.user.user_id = response.data.user_id;
+        		$scope.user.business_name = response.data.business_name;
+        		$scope.user.owner_name = response.data.owner_name;
+        		$scope.user.bank_acc = response.data.bank_acc;
+        		$rootScope.name =  $scope.user.owner_name;
 			} else {
 				$scope.user.autherror = "Invalid User ID or Password";
 				$scope.user.authenticationError = false;
